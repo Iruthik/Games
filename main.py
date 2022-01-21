eff7d42d22972ab118b4a9a8211b7d00194f75a3
Iruthik <iruthik.sankar@gmail.com>
2022-01-21 09:28:31 +0530

    score error really solved

--- main.py
+++ main.py
@@ -113,8 +113,11 @@ while True:
             high_score = score
             pen.clear()
             pen.write("Score = {} High Score = {}".format(score,high_score),align="center",font=("Courier",20,"normal"))
+
+        pen.clear()
         pen.write("Score = {} High Score = {}".format(score, high_score), align="center",
                   font=("Courier", 20, "normal"))
+
 
     for index in range(len(segments)-1,0,-1):
         x = segments[index-1].xcor()
